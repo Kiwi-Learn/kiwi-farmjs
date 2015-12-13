bf1afa0daa77112fcadf34ba1bf4d8440467c5fb
Che-Wei Lin <linton.tw@gmail.com>
2015-12-13 16:34:25 +0800

    test cases

--- test/app-spec.js
+++ test/app-spec.js
@@ -4,6 +4,14 @@ let should = require('should');
 let express = require('express');
 let app = require('../app');
 
+describe('Kiwi FarmJS root route', () => {
+  it('should return root route without 404', (done) => {
+    supertest(app)
+      .get('/')
+      .expect(200, done);
+  });
+});
+
 describe('Kiwi FarmJS API Tests', () => {
   it('should return home page', (done) => {
     supertest(app)
